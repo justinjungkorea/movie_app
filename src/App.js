6ba89d47beafb1bc1f157b7c04e638c5d430f1f7
Justin_Jung <jungdw0624@gmail.com>
2019-04-23 20:12:38 +0900

    state!

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
 import React, { Component } from 'react';
 import './App.css';
-import Movie from './Movie.js';
-
-const movies = [
-  {
-    title: "Iron Man",
-    poster: "https://movie-phinf.pstatic.net/20111222_37/1324501632182vbSoY_JPEG/movie_image.jpg"
-  },
-  {
-    title: "Transformer",
-    poster: "https://movie-phinf.pstatic.net/20111223_180/1324583623164LA0FV_JPEG/movie_image.jpg"
-  },
-  {
-    title: "National Treasure",
-    poster: "https://movie-phinf.pstatic.net/20111221_116/1324452697896DMgIP_JPEG/movie_image.jpg"
-  },
-  {
-    title: "Ocean's eleven",
-    poster: "https://movie-phinf.pstatic.net/20111222_295/1324531366643q7BRh_JPEG/movie_image.jpg"
-  },
-  {
-    title: "Italian job",
-    poster: "https://movie-phinf.pstatic.net/20111221_192/1324475568659ItkXm_JPEG/movie_image.jpg"
-  }
-];
+import Movie from './Movie';
 
 class App extends Component {
+  state = {
+    
+  };
+
+  componentDidMount() {
+    setTimeout(()=>{
+      this.setState({
+        movies: [
+          {
+            title: "Iron Man",
+            poster:
+              "https://movie-phinf.pstatic.net/20111222_37/1324501632182vbSoY_JPEG/movie_image.jpg"
+          },
+          {
+            title: "Transformer",
+            poster:
+              "https://movie-phinf.pstatic.net/20111223_180/1324583623164LA0FV_JPEG/movie_image.jpg"
+          },
+          {
+            title: "National Treasure",
+            poster:
+              "https://movie-phinf.pstatic.net/20111221_116/1324452697896DMgIP_JPEG/movie_image.jpg"
+          },
+          {
+            title: "Ocean's eleven",
+            poster:
+              "https://movie-phinf.pstatic.net/20111222_295/1324531366643q7BRh_JPEG/movie_image.jpg"
+          },
+          {
+            title: "Italian job",
+            poster:
+              "https://movie-phinf.pstatic.net/20111221_192/1324475568659ItkXm_JPEG/movie_image.jpg"
+          },
+          {
+            title: "Tranporter",
+            poster:
+              "https://movie-phinf.pstatic.net/20111222_261/1324487946607k7Dwz_JPEG/movie_image.jpg"
+          }
+        ]
+      });
+    },5000)
+  }
+
+  _rederMovies = () =>{
+    const movies = this.state.movies.map((movie, index) => {
+      return (
+        <Movie title={movie.title} poster={movie.poster} key={index} />
+      );
+    });
+
+    return movies;
+  }
+
   render() {
     return (
       <div className="App">
-        {movies.map(movie => {
-          return <Movie title = {movie.title} poster={movie.poster} />
-        })}
+        {this.state.movies ? this._rederMovies() : 'Loading...'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import "./Movie.css";
 
 class Movie extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string
+  };
+
   render() {
     return (
       <div>
-        <MoviePoster poster={this.props.poster}/>
+        <MoviePoster poster={this.props.poster} />
         <h1>{this.props.title}</h1>
       </div>
     );
@@ -13,9 +19,14 @@ class Movie extends Component {
 }
 
 class MoviePoster extends Component {
+
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+
   render() {
     return (
-        <img src={this.props.poster}/>
+        <img src={this.props.poster} width="200px"/>
     );
   }
 }
