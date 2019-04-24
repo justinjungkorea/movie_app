305b9651a04a854bcef3f74b879d667a4dee34a4
Justin_Jung <jungdw0624@gmail.com>
2019-04-25 00:07:44 +0900

    Updates

---
+++ precache-manifest.898cfae2045027c11e5c0ef7ceb1ff29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36f2eeeeac9589c19c20",
+    "url": "/movie_app/static/css/main.825e4608.chunk.css"
+  },
+  {
+    "revision": "36f2eeeeac9589c19c20",
+    "url": "/movie_app/static/js/main.dcc0ef7d.chunk.js"
+  },
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "6303b1dc6d95fb6846a7",
+    "url": "/movie_app/static/js/2.9dee5afb.chunk.js"
+  },
+  {
+    "revision": "c99ffa68b546b5626f968d5b399e9ef0",
+    "url": "/movie_app/index.html"
+  }
+];
