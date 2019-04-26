0ac0e054724c55cdd7ffc38dde02e63630869288
Justin_Jung <jungdw0624@gmail.com>
2019-04-26 12:23:58 +0900

    Updates

---
+++ precache-manifest.2155a14f16af450cae7af2b796b983b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "037ee066ced0e4a5a208",
+    "url": "/movie_app/static/css/main.f72f43f6.chunk.css"
+  },
+  {
+    "revision": "037ee066ced0e4a5a208",
+    "url": "/movie_app/static/js/main.d00232b7.chunk.js"
+  },
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "a98868795fdba2913106",
+    "url": "/movie_app/static/js/2.421ca046.chunk.js"
+  },
+  {
+    "revision": "21b1b948e5b67852ed61c32ec3e08eec",
+    "url": "/movie_app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.1f9dcdf050fda00dab3e5da3c8850ad1.js"
+  "/movie_app/precache-manifest.2155a14f16af450cae7af2b796b983b0.js"
 );
 
 workbox.clientsClaim();
